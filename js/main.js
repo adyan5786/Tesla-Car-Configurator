1d67e422563abd0e1d65565e906dbebe6dcc3e7d
Adyan Shaikh <adyanshaikh2020@gmail.com>
2026-02-10 00:39:34 +0530

    Add Top Bar Animation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const topBar = document.querySelector('#top-bar');
+
+// Handle Top Bar on Scrolls
+const handleScroll = () => {
+    const atTop = window.scrollY === 0;
+
+    topBar.classList.toggle('visible-bar', atTop);
+    topBar.classList.toggle('hidden-bar', !atTop);
+};
+
+// Event Listeners
+window.addEventListener('scroll', () => requestAnimationFrame(handleScroll))
