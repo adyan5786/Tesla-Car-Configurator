943a3231e672345877bd7de9b5b493b9818dca6e
Adyan Shaikh <adyanshaikh2020@gmail.com>
2026-02-11 21:36:50 +0530

    Handle Colour Selection

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const topBar = document.querySelector('#top-bar');
+const exteriorColorSection = document.querySelector('#exterior-buttons');
+const interiorColorSection = document.querySelector('#interior-buttons');
+const exteriorImage = document.querySelector('#exterior-image');
+const interiorImage = document.querySelector('#interior-image');
 
 // Handle Top Bar on Scrolls
 const handleScroll = () => {
@@ -8,5 +12,51 @@ const handleScroll = () => {
     topBar.classList.toggle('hidden-bar', !atTop);
 };
 
+// Image Mapping
+const exteriorImages = {
+    'Stealth Grey': './images/model-y-stealth-grey.jpg',
+    'Pearl White': './images/model-y-pearl-white.jpg',
+    'Deep Blue Metallic': './images/model-y-deep-blue-metallic.jpg',
+    'Solid Black': './images/model-y-solid-black.jpg',
+    'Ultra Red': './images/model-y-ultra-red.jpg',
+    'Quicksilver': './images/model-y-quicksilver.jpg',
+}
+
+const interiorImages = {
+    'Dark' : './images/model-y-interior-dark.jpg',
+    'Light' : './images/model-y-interior-light.jpg',
+}
+
+// Handle Colour Selection
+const handleColourButtonClick = (event) => {
+    let button;
+
+    if (event.target.tagName === "IMG") {
+        button = event.target.closest('button');
+    } else if (event.target.tagName === "BUTTON") {
+        button = event.target;
+    }
+
+    if (button) {
+        const buttons = event.currentTarget.querySelectorAll('button');
+        buttons.forEach((btn) => btn.classList.remove('btn-selected'));
+        button.classList.add('btn-selected');
+
+        // Change exterior image
+        if (event.currentTarget === exteriorColorSection) {
+            const color = button.querySelector('img').alt;
+            exteriorImage.src = exteriorImages[color];
+        }
+
+        // Change interior image
+        if (event.currentTarget === interiorColorSection) {
+            const color = button.querySelector('img').alt;
+            interiorImage.src = interiorImages[color];
+        }
+    }
+}
+
 // Event Listeners
 window.addEventListener('scroll', () => requestAnimationFrame(handleScroll))
+exteriorColorSection.addEventListener('click', handleColourButtonClick);
+interiorColorSection.addEventListener('click', handleColourButtonClick);
